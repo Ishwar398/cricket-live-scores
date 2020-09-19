2920e31bd8818c8afe99d9606227bf5299845109
Ishwar Patil <patilishwar760@hotmail.com>
2020-09-20 00:17:41 +0530

    First commit for 4 methods

---
+++ application.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template
+from werkzeug.exceptions import Forbidden, HTTPException, NotFound, RequestTimeout, Unauthorized
+from pycricbuzz import Cricbuzz
+import json
+
+c=Cricbuzz()
+app = Flask(__name__)
+
+
+@app.route("/GetMatchDetails",methods=['POST'])
+def GetMatchDetails():
+    matchId=request.form.get('matchId')
+    jsonObj_matchInfo = c.matchinfo(matchId)
+    return json.dumps({'success':True,'data':jsonObj_matchInfo}), 200, {'ContentType':'application/json'} 
+
+@app.route("/GetMatchLiveScore",methods=['POST'])
+def GetMatchLiveScore():
+    matchId=request.form.get('matchId')
+    jsonObj_matchLiveScore = c.livescore(matchId)
+    return json.dumps({'success':True,'data':jsonObj_matchLiveScore}), 200, {'ContentType':'application/json'} 
+
+@app.route("/GetMatchScoreCard",methods=['POST'])
+def GetMatchScoreCard():
+    matchId=request.form.get('matchId')
+    jsonObj_matchScoreCard = c.scorecard(matchId)
+    return json.dumps({'success':True,'data':jsonObj_matchScoreCard}), 200, {'ContentType':'application/json'} 
+
+@app.route("/GetMatchCommentry",methods=['POST'])
+def GetMatchCommentry():
+    matchId=request.form.get('matchId')
+    jsonObj_matchCommentry = c.commentary(matchId)
+    return json.dumps({'success':True,'data':jsonObj_matchCommentry}), 200, {'ContentType':'application/json'} 
+
+@app.route("/GetOnGoingMatches",methods=['POST'])
+def GetOnGoingMatches():
+    matches = c.matches()
+    return json.dumps({'success':True,'data':matches}), 200, {'ContentType':'application/json'} 
+
+@app.errorhandler(NotFound)
+def page_not_found_handler(e: HTTPException):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(Unauthorized)
+def unauthorized_handler(e: HTTPException):
+    return render_template('401.html'), 401
+
+
+@app.errorhandler(Forbidden)
+def forbidden_handler(e: HTTPException):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(RequestTimeout)
+def request_timeout_handler(e: HTTPException):
+    return render_template('408.html'), 408
+
+
